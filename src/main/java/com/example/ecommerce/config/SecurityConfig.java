1d4544753477b6574b9a5f2801ccf1323b9fb6d3
Rupali Dalvi <rupalidalvi0932@gmail.com>
2026-09-17 21:12:27 +0530

    basic authentication for deployment

--- src/main/java/com/example/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/example/ecommerce/config/SecurityConfig.java
@@ -2,7 +2,7 @@ package com.example.ecommerce.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -33,9 +33,8 @@ public class SecurityConfig {
                                 "/swagger-ui.html",
                                 "/v3/api-docs/**"
                         ).permitAll()
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(Customizer.withDefaults());
+                        .anyRequest().permitAll()
+                );
 
         return http.build();
     }
